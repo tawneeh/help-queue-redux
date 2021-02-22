5523a0c25e6ba9899072e299e2baf3ab424b1e83
Tawnee Harris <tawneeh@icloud.com>
2021-02-22 11:37:57 -0800

    update tests to account for Firestore

--- src/__tests__/reducers/ticket-list-reducer.test.js
+++ src/__tests__/reducers/ticket-list-reducer.test.js
@@ -28,28 +28,28 @@ describe('ticketListReducer', () => {
     expect(ticketListReducer({}, { type: null })).toEqual({});
   });
 
-test('should successfully add a ticket to the ticket list that includes Moment-formatted wait times', () => {
-    const { names, location, issue, timeOpen, id } = ticketData;
-    action = {
-      type: c.ADD_TICKET,
-      names: names,
-      location: location,
-      issue: issue,
-      timeOpen: timeOpen,
-      id: id,
-      formattedWaitTime: new Moment().fromNow(true)
-    };
-    expect(ticketListReducer({}, action)).toEqual({
-      [id] : {
-        names: names,
-        location: location,
-        issue: issue,
-        timeOpen: timeOpen,
-        id: id,
-        formattedWaitTime: 'a few seconds'
-      }
-    });
-  });
+// test('should successfully add a ticket to the ticket list that includes Moment-formatted wait times', () => {
+//     const { names, location, issue, timeOpen, id } = ticketData;
+//     action = {
+//       type: c.ADD_TICKET,
+//       names: names,
+//       location: location,
+//       issue: issue,
+//       timeOpen: timeOpen,
+//       id: id,
+//       formattedWaitTime: new Moment().fromNow(true)
+//     };
+//     expect(ticketListReducer({}, action)).toEqual({
+//       [id] : {
+//         names: names,
+//         location: location,
+//         issue: issue,
+//         timeOpen: timeOpen,
+//         id: id,
+//         formattedWaitTime: 'a few seconds'
+//       }
+//     });
+//   });
 
     test('Should successfully delete a ticket', () => {
     action = {
